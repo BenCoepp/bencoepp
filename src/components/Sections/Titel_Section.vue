979122cfcb537b08d17d1bc464575b35232f4b6b
BenCoepp <bencoepp@gmail.com>
2021-01-24 18:44:59 +0100

    added all nessesary files

---
+++ src/components/Sections/Titel_Section.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container fluid class="titelWrapper">
+    <b-row>
+      <b-col class="col">1</b-col>
+      <b-col class="col">2</b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Titel_Section",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.titelWrapper{
+  width: 100%;
+  height: 100vh;
+}
+.col{
+  width: 100%;
+  height: 100vh;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Titel_Section msg="Welcome to Your Vue.js App" />
+    <WhatIDo_Section msg="Welcome to Your Vue.js App" />
+    <Resume_Section msg="Welcome to Your Vue.js App" />
+    <b-container fluid>
+      <AGuidetoQt6_Project msg="Project 1" />
+      <Cherry_Project msg="Project 2" />
+    </b-container>
+    <Disclaimer msg="Welcome to Your Vue.js App" />
+    <Contact_Section msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Titel_Section from "@/components/Sections/Titel_Section.vue";
+import WhatIDo_Section from "@/components/Sections/WhatIDo_Section.vue";
+import Resume_Section from "@/components/Sections/Resume_Section.vue";
+import Contact_Section from "@/components/Sections/Contact_Section.vue";
+import Disclaimer from "@/components/Disclaimer.vue";
 
+import AGuidetoQt6_Project from "@/components/Projects/AGuidetoQt6_Project.vue";
+import Cherry_Project from "@/components/Projects/Cherry_Project.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Titel_Section,
+    WhatIDo_Section,
+    Resume_Section,
+    Contact_Section,
+    Disclaimer,
+    AGuidetoQt6_Project,
+    Cherry_Project
   }
 };
 </script>
