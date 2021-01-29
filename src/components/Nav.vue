d62bf21a66b01e55b9f38cc0a5518e8e81a3602f
BenCoepp <bencoepp@gmail.com>
2021-01-29 22:52:57 +0100

    added fixed navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,19 @@
 <template>
-  <b-container fluid class="navWrapper">
-    <b-avatar src="https://placekitten.com/300/300" id="logo"></b-avatar>
-    <h2 id="companyName">Ben Coepp</h2>
-    <router-link class="routerLink" to="/">Portfolio</router-link>
-    <router-link class="routerLink" to="/about">My Stuff</router-link>
-    <router-link class="routerLink" to="/products">Products</router-link>
-  </b-container>
+  <fixed-header>
+    <div class="navbar">
+      Your Content
+    </div>
+  </fixed-header>
 </template>
 
 <script>
+import FixedHeader from 'vue-fixed-header'
+
 export default {
-  name: "Disclaimer",
+  name: "Nav",
+  components: {
+    FixedHeader
+  },
   props: {
     msg: String
   }
@@ -19,24 +22,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.navWrapper{
-  width: 100%;
-  background-color: #fd7e35;
-  height: 50px;
-  display: inline-flex;
+.navbar.vue-fixed-header--isFixed {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
 }
-#logo{
-    margin: 3px;
-}
-#companyName{
-    font-weight: bold;
-    color: white;
-    margin-left: 10px;
-}
-.routerLink{
-    margin-top: 10px;
-    margin-left: 1em;
-    font-weight: bold;
-    color: white;
+.navbar{
+  background-color: #3e5a79;
 }
 </style>
--- src/components/Sections/Contact_Section.vue
+++ src/components/Sections/Contact_Section.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <b-container fluid class="contactWrapper">
+    <b-row>
+      <b-col>
+        <b-row>
+          <h1>Let's get in touch</h1>
+          <h2>You can contact me when ever you want just go ahead</h2>
+        </b-row>
+        <b-row>
+          <b-col id="telCardCol">
+             <b-card class="telCard">
+              <b-icon-telephone id="telCardIcon"></b-icon-telephone>
+              <b-card-text>Please only use when contacting for buying reasons</b-card-text>
+              <b-card-text>0173 4066728</b-card-text>
+            </b-card>
+            <b-card class="telCard">
+              <b-icon-text-left id="telCardIcon"></b-icon-text-left>
+              <b-card-text>Please only use when contacting for buying reasons</b-card-text>
+              <b-card-text>0173 4066728</b-card-text>
+            </b-card>
+            <b-card class="telCard">
+              <b-icon-emoji-sunglasses id="telCardIcon"></b-icon-emoji-sunglasses>
+              <b-card-text>Please only use when contacting for buying reasons</b-card-text>
+              <b-card-text>0173 4066728</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-icon-triangle-fill rotate="-90" id="arrawIcon"></b-icon-triangle-fill>
+      </b-col>
+      <b-col id="rightColContact">
+         <b-icon-triangle-fill rotate="-90" id="arrawIcon"></b-icon-triangle-fill>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -15,5 +45,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.contactWrapper{
+  height: 100vh;
+  background-color: #3e5a79;
+  margin: 0;
+}
+#telCardCol{
+  margin: 10%;
+}
+.telCard{
+  float: right;
+  margin-bottom: 1em;
+  width: 60%;
+  background-color: #3e5a79;
+  color: white;
+  -webkit-box-shadow: 5px 5px 19px 7px rgba(62,90,121,0.82); 
+  box-shadow: 5px 5px 19px 7px rgba(62,90,121,0.82);
+}
+#telCardIcon{
+  width: 40px;
+  height: 40px;
+  color: white;
+}
+#rightColContact{
+  background-color: #fd7e35;
+  height: 100vh;
+}
+#arrawIcon{
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  top: 10%;
+  left: 96%;
+  color: #fd7e35;
+}
 </style>
