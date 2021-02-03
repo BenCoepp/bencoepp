e9a31afb8547b57da12c93e25a5612240cfc0001
BenCoepp <bencoepp@gmail.com>
2021-02-03 19:39:47 +0100

    nav update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,13 @@
   <b-navbar toggleable="lg" type="dark" class="nav">
     <b-navbar-brand href="#" id="brandTitel">
       <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="Kitten">
-      Ben Coepp
+      <router-link to="/" class="rLink">Ben Coepp</router-link>
     </b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <router-link to="/about">About</router-link>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <router-link to="/about" class="rLink">My Stuff</router-link>
+        <router-link to="/about" class="rLink">Products</router-link>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -33,5 +33,16 @@ export default {
 }
 #brandTitel{
   font-weight: bold;
+}
+#brandTitel:hover{
+  color: #fd7e35;
+}
+.rLink{
+  margin-right: 1em;
+  color: white;
+}
+.rLink:hover{
+  text-decoration: none;
+  color: #fd7e35;
 }
 </style>
